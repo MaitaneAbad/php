9e8577c1320f7d53e76d4863cd1cb93264439d1c
Maitane Abad <mabad@dmi.es>
2022-04-19 11:15:28 +0200

    firts commit

---
+++ incrustar.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Maitane";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INCRUSTAR PHP CON HTML</title>
+</head>
+<body>
+    <h1>Mi nombre es <?php echo $name ?> </h1>
+    <p><?php print("Hola mundo")?></p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+// !VARIABLES
+
+// *CONCATENACIÓN STRINGS
+// $variable1 = 'Mai Abad';
+// $variable2 = 'Conde';
+
+// echo $variable1 . ' ' . $variable2;
+
+
+// *OPERACIONES NÚMERICAS
+// $variable3 = 3;
+// $variable4 = 4;
+
+// $suma = $variable3 + $variable4;
+// echo $suma;
+
+// !CONSTANTES
+
+// define("numero1", 5);
+// define("numero2", 8);
+
+// $suma = numero1 + numero2;
+// echo $suma;
+
+// !BUCLE FOR Y FOREACH
+
+// *FOR
+//  $nombre = "Mai";
+//  $array = array(1, 2, 3, "casa", $nombre);
+
+//  //saco el numero de elementos
+//  $longitud = count($array);
+
+//  //Recorro todos los elementos
+//  for($i=0; $i<$longitud; $i++){
+//      //saco el valor de cada elemento  
+//     echo $array[$i]."<br>";
+
+//  }
+
+
+// *FOREACH
+// $array2 = array('uno', 'dos', 'tres');
+
+// foreach($array2 as $key => $value){
+//     echo $value;
+//     echo "<br>";
+// }
+
+// !FUNCIONES
+
+// *FUNCION BÁSICA
+// function bucleFor(){
+// $array2 = array('uno', 'dos', 'tres');
+
+//     foreach($array2 as $key => $value){
+//         echo $value;
+//         echo "<br>";
+//     }
+// }
+
+// bucleFor();
+
+// *FUNCION CON PARÁMETROS
+// function suma($a, $b){
+//     $total = $a + $b;
+//     echo $total;
+// }
+// suma(6,8);
+?>
